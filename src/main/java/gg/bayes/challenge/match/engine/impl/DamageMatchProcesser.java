86e08439ac88e447409d409d785a827c784909b1
Ajay Laddha <ajay.laddha4@gmail.com>
2022-06-05 19:29:20 +0000

    optimised invalid matched

--- src/main/java/gg/bayes/challenge/match/engine/impl/DamageMatchProcesser.java
+++ src/main/java/gg/bayes/challenge/match/engine/impl/DamageMatchProcesser.java
@@ -3,6 +3,7 @@ package gg.bayes.challenge.match.engine.impl;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import gg.bayes.challenge.match.engine.MatchProcesser;
@@ -11,7 +12,7 @@ import gg.bayes.challenge.match.engine.MatchProcesser;
 @Component
 public class DamageMatchProcesser implements MatchProcesser{
 	
-	private Pattern pattern = Pattern.compile("\\[\\b(.*)\\] \\b(.*) \\b(hits\\s*) \\b(.*) \\b(with) \\b(.*) \\b(for) \\b(.*) \\b(damage)\\b \\((.*)->(.*)\\)");
+	private Pattern pattern = Pattern.compile("\\[\\b(.*)\\] \\b(.*) \\b(hits\\s*) \\b(.*) \\b(with) \\b(.*) \\b(for) \\b(.*) \\b(damage)\\b(.*)");
 	
 	private String queryTemplate = "insert into dim_damage_events (match_id, timestamp, hero, damaged_hero, damaged_weapon, damage_score, score_before_damage, score_after_damage) values (\'%1$s\', \'%2$s\', \'%3$s\', \'%4$s\', \'%5$s\',\'%6$s\',\'%7$s\',\'%8$s\')";
 	@Override
@@ -33,8 +34,17 @@ public class DamageMatchProcesser implements MatchProcesser{
     	String opHero = matcher.group(4).replace("npc_dota_hero_", "");
     	String damage_weapon = matcher.group(6);
     	String damageScore = matcher.group(8);
-    	String scoreBefore = matcher.group(10);
-    	String scoreAfter = matcher.group(11);
+    	String scoreBeforeAfter = matcher.group(10);
+    	String scoreBefore = "-1";
+    	String scoreAfter = "-1";
+    	if(!StringUtils.isBlank(matcher.group(10))) {
+    		try {
+    			String[] beforeAfterSplit = scoreBeforeAfter.trim().replace("(", "").replace(")", "").split("->");
+    			// System.out.println(beforeAfterSplit[0]);
+        		scoreBefore = beforeAfterSplit[0];
+        		scoreAfter = beforeAfterSplit[1];
+			} catch (Exception e) {}
+    	}
     	return String.format(getDBQueryTemplate(), matchId, timestamp, hero, opHero, damage_weapon, damageScore, scoreBefore, scoreAfter);
     }
 }
